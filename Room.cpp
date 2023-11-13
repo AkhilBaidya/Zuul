8f9f1089cc0f9fe7541c8370da59994b32cb21f0
U-BSD\463541 <463541@SHS-CYBF6X3.beaverton.k12.or.us>
2023-11-13 10:30:17 -0800

    Beginning to define methods

--- Room.cpp
+++ Room.cpp
@@ -7,3 +7,25 @@
 
 using namespace std:
 
+Room::Room(char[50] theTitle, char[200] theDescription, vector<struct* item> theItems) {
+} //constructor
+
+Room::~Room() {
+} //destructor
+
+//get methods:
+char* Room::getTitle(){
+}
+char* Room::getDescription();{
+}
+vector<struct* item> Room::getItems(); {
+}
+
+//set methods:
+void Room::setTitle(char[50] newTitle){
+}
+void Room::setDescription(char[200] newDescription){
+}
+void Room::setItems(vector<struct* item> newItems){
+}
+ 
