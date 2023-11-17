1f782cb53d2127f626347da4ddca638ca5b2ccda
U-BSD\463541 <463541@SHS-CYBF6X3.beaverton.k12.or.us>
2023-11-17 10:39:00 -0800

    fixing how to access fields in struct and setting those fields with strcpy

--- Main.cpp
+++ Main.cpp
@@ -5,9 +5,25 @@
 #include <vector>
 #include "Room.h"
 
-using namespace std:
+using namespace std;
 
 int main(){
 
+  char title[50] = "Home";
+  char description[200] = "Pretty nice here";
+
+  vector<Item*> items;
+  Item* Bob = new Item;
+  strcpy(Bob -> itemTitle, "Cool orb");
+  strcpy(Bob -> itemDes, "Shines brilliantly");
+  items.push_back(Bob);
+
+  vector<char*> exits;
+  char* north = new char[10];
+  strcpy(north, "NORTH");
+  exits.push_back(north);
+  
+  Room(title, description, items, exits);
+  
   return 0;
 }
