9512731033db4a4222ac49cb55a4cb344be7ab41
U-BSD\463541 <463541@SHS-CYBF6X3.beaverton.k12.or.us>
2023-11-17 09:54:25 -0800

    stated get and set methods

--- Item.h
+++ Item.h
@@ -13,6 +13,14 @@ class Item {
   Item(char[50] theTitle, char[200] theDescription); //constructor
   ~Item(); //destructor
 
+  //get methods:
+  char* getTitle();
+  char* getDescription();
+
+  //set methods:
+  void setTitle(char[50] newTitle);
+  void setDescription(char[200] newDescription);
+
  protected: //fields unique to an item
   char* title;
   char* description; 
