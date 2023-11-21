2c911c2862a97c917e9779e16a10ff785d94fdfc
Akhil Baidya <akhilbaidya101@gmail.com>
2023-11-21 11:30:37 -0800

    fixed errors in constructor

--- Room.cpp
+++ Room.cpp
@@ -4,14 +4,16 @@
 #include <cstring>
 #include <vector>
 #include "Room.h"
+#include <map>
 
 using namespace std;
 
 Room::Room(char theTitle[50], char theDescription[200], vector<Item*> theItems) {
+  title = new char[50];
+  description = new char[200];
   strcpy(title, theTitle);
   strcpy(description, theDescription); //want to copy so I don't set two char* equal to each other (gave me errors in Classes project)
-  items = theItems;
-  map<char*, Room> exits = new map<char*, Room>; 
+  items = theItems; 
 } //constructor
 
 Room::~Room() {
